51e99c0dd76f5aa58d216022399531929ab40e20
SignificantNose <yanucevich.d@gmail.com>
2022-12-10 18:55:58 +0300

    с шаблоном для русского

--- theBase.py
+++ theBase.py
@@ -1,10 +1,23 @@
 from random import randint
 
-f = open('Dictionary.txt')
-dict = f.readline()
-dict = dict.split(' ')
-LEFT_BORDERS = [0,52,104,304,504,704,904,1104,1304,1504]
-RIGHT_BORDERS = [51,103,303,503,703,903,1103,1303,1503,1703]
+
+# 1 - это англ, 0 - это русский
+language = 1
+if language:
+    f = open('Dictionary.txt')
+    dict = f.readline()
+    dict = dict.split(' ')
+    LEFT_BORDERS = [0, 52, 104, 304, 504, 704, 904, 1104, 1304, 1504]
+    RIGHT_BORDERS = [51, 103, 303, 503, 703, 903, 1103, 1303, 1503, 1703]
+    bool = [0] * 1704
+else:
+    #   f = open('      Dictionary FOR RUSSIAN   .txt')
+    # dict = f.readline()
+    # dict = dict.split(' ')
+    LEFT_BORDERS = [0,    66,     132,332,532,732,932,1132,1332,1532]
+    RIGHT_BORDERS = [65,    131,    331,531,731,931,1131,1331,1531,1731]
+    bool = [0] * 1732
+
 
 currWord = []
 def find_word(currLength):
@@ -14,12 +27,15 @@ def find_word(currLength):
         else:
             i = randint(1,currLength)-1
         indexForDict = randint(LEFT_BORDERS[i],RIGHT_BORDERS[i])
-        currWord.append(dict[indexForDict])
-        currLength-=i+1
+        if bool[indexForDict]:
+            pass
+        else:
+            currWord.append(dict[indexForDict])
+            currLength-=i+1
+            bool[indexForDict] = 1
     return currWord
+
+# INSERT THE LENGTH HERE
 find_word(20)
 print(currWord)
-
-
-print(len(dict))
 f.close()
