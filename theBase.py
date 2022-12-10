5632fb95b05f5258646373ce071cec6e1acacea3
SignificantNose <yanucevich.d@gmail.com>
2022-12-10 18:12:02 +0300

    с функцией подбора по длине (там по автомату указано 20)

--- theBase.py
+++ theBase.py
@@ -1,10 +1,24 @@
+from random import randint
+
 f = open('dictionary.txt')
 dict = f.readline()
 dict = dict.split(' ')
+LEFT_BORDERS = [0,52,104,304,504,704,904,1104,1304,1504]
+RIGHT_BORDERS = [51,103,303,503,703,903,1103,1303,1503,1703]
 
-
-
-
+currWord = []
+def find_word(currLength):
+    while currLength!=0:
+        if currLength >= 10:
+            i = randint(1,10)-1
+        else:
+            i = randint(1,currLength)-1
+        indexForDict = randint(LEFT_BORDERS[i],RIGHT_BORDERS[i])
+        currWord.append(dict[indexForDict])
+        currLength-=i+1
+    return currWord
+find_word(20)
+print(currWord)
 
 print(len(dict))
 f.close()
